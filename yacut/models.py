f206f47fea65a25f37e47ba19c0e5242a36c0108
Иван Сиснёв <ivan.sisnev@gmail.com>
2023-09-15 11:48:01 +0300

    Описать модель URLMap

--- yacut/models.py
+++ yacut/models.py
@@ -1,7 +1,17 @@
+"""
+Модели проекта.
+"""
+
 from datetime import datetime
 
 from yacut import db
 
 
 class URLMap(db.Model):
-    ...
+    """Модель БД проекта: таблица url адресов."""
+    id = db.Column(db.Integer, primary_key=True)
+    # поле оригинальной ссылки
+    original = db.Column(db.String(), nullable=False)
+    # поле короткого alias
+    short = db.Column(db.String(16), unique=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
