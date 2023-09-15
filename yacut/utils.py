162ab0e74406fa82751917dad02dd80b2113ce6b
Иван Сиснёв <ivan.sisnev@gmail.com>
2023-09-15 21:10:47 +0300

    Сделать проверку short_id на дубликаты, реализовать сохранение записей в БД

--- yacut/utils.py
+++ yacut/utils.py
@@ -4,12 +4,25 @@
 from random import choices
 
 from settings import CUSTOM_ID_SEQUENCE, CUSTOM_ID_LENGTH
+from yacut.models import URLMap
 
 
 def get_unique_short_id() -> str:
     """
     Создает строку заданной длины из символов, выбранных случайным
-    образом из заданной последовательности.
+    образом из заданной последовательности. Проверяет ее на уникальность.
     :return: строка
     """
-    return ''.join(choices(CUSTOM_ID_SEQUENCE, k=CUSTOM_ID_LENGTH))
+    short_id: str = ''.join(choices(CUSTOM_ID_SEQUENCE, k=CUSTOM_ID_LENGTH))
+    # если такая же строка уже есть в БД
+    if not check_for_duplicates(short_id):
+        get_unique_short_id()
+    return short_id
+
+
+def check_for_duplicates(short_id: str) -> bool:
+    """
+    Проверяет короткую ссылку на уникальность в БД.
+    :return: True ссылка уникальна.
+    """
+    return URLMap.query.filter_by(short=short_id).first() is None
--- yacut/views.py
+++ yacut/views.py
@@ -2,20 +2,40 @@
 View-функции приложения.
 """
 from flask.views import View
-from flask import render_template, request
+from flask import request, render_template, redirect, flash
 
 from yacut import app, db
 from yacut.models import URLMap
 from yacut.forms import UrlForm
+from yacut.utils import get_unique_short_id, check_for_duplicates
 
 
 class IndexPage(View):
-    methods = ["GET", "POST"]
+    """
+    View-класс главной страницы.
+    """
+    methods = ['GET', 'POST']
 
     def dispatch_request(self):
+        """
+        View-функция.
+        """
         form = UrlForm()
-        if request.method == 'POST':
-            ...
+        # todo попробовать убрать проверку метода
+        if request.method == 'POST' and form.validate_on_submit():
+            short_id = form.custom_id.data
+            if not short_id:
+                short_id: str = get_unique_short_id()
+            elif not check_for_duplicates(short_id):
+                flash('Такая короткая ссылка уже используется')
+                # todo оставить original_link
+                return render_template('index_page.html', form=form)
+            urlmap = URLMap(original=form.original_link.data, short=short_id)
+            db.session.add(urlmap)
+            db.session.commit()
+            # todo оставить original_link и short_id если было
+            # todo вывести готовую ссылку
+            return redirect(...)
         return render_template('index_page.html', form=form)
 
 
