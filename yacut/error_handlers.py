c0fee4d3b317060ef2da77d0476ddf83d61f4c6b
Иван Сиснёв <ivan.sisnev@gmail.com>
2023-09-18 11:55:09 +0300

    Добавить обработчики ошибок 404 и 500

--- yacut/error_handlers.py
+++ yacut/error_handlers.py
@@ -1,3 +1,25 @@
 """
 Обработчики ошибок приложения.
 """
+from flask import jsonify, render_template
+
+from . import app, db
+
+# todo проверить работу обработчиков.
+@app.errorhandler(404)
+def page_not_found(error):
+    """
+    Обработчик ошибки 404.
+    """
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    """
+    Обработчик ошибки 500.
+    """
+    db.session.rollback()
+    return render_template('500.html'), 500
+
+# todo добавить обработчики ошибок API
